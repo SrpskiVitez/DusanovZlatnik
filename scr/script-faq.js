24efae7d3e0b629c3c7cea2706dfb9d29a73b589
SrpskiVitez <76843998+SrpskiVitez@users.noreply.github.com>
2025-08-10 07:33:15 +0200

    d

--- scr/script-faq.js
+++ scr/script-faq.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.lang-section ol li strong').forEach((question) => {
-  question.addEventListener('click', () => {
+  question.addEventListener('click', (e) => {
+    e.stopPropagation(); // da ne kaska više događaja
     const li = question.parentElement;
     li.classList.toggle('open');
   });
